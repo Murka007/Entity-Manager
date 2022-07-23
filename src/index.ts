4b1d69e6e8c7e76768c54429309f4b1df55959c6
Parzanga <54054900+Parzanga@users.noreply.github.com>
2022-07-24 03:08:26 +0400

    added files

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+console.log(canvas);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const Terser = require("terser-webpack-plugin");
+
+const isProd = process.env.MODE === "production";
+module.exports = {
+    mode: process.env.MODE,
+    target: ["web", "es5"],
+    context: path.resolve(__dirname, "src"),
+    entry: "./index.ts",
+    output: {
+        filename: "[contenthash:8].js",
+        path: path.resolve(__dirname, "public"),
+        clean: true
+    },
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    optimization: {
+        minimizer: [
+            new Terser()
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "[contenthash:8].html",
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: isProd,
+                minifyCSS: isProd
+            }
+        })
+    ],
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: "ts-loader",
+            exclude: /node-modules/
+        }]
+    }
+}
